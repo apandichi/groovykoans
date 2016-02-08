80041807e6ce63f98734cf73663373c7f60b1a31
Alin Pandichi <alin.pandichi@gmail.com>
2016-02-08 11:30:04 +0200

    Finished koan 06

--- src/test/groovy/org/groovykoans/koan06/Koan06.groovy
+++ src/test/groovy/org/groovykoans/koan06/Koan06.groovy
@@ -34,7 +34,12 @@ class Koan06 extends GroovyTestCase {
         // Groovy-fy the above code, using StringBuilder and with() to get the same result in Groovy
         String groovyResult
         // ------------ START EDITING HERE ----------------------
-
+        groovyResult = new StringBuilder().with {
+            append("roses are #FF0000\\n")
+            append("violets are #0000FF\\n")
+            append("all my base\\n")
+            append("are belong to you\\n")
+        }
 
         // ------------ STOP EDITING HERE  ----------------------
         assert groovyResult == javaResult
@@ -50,8 +55,9 @@ class Koan06 extends GroovyTestCase {
         def differentTypes = [1, 'String', "GString", 'a', 'Another string', 0]
         def uniqueTypes = []
         // ------------ START EDITING HERE ----------------------
-
-
+        uniqueTypes = differentTypes.collect({
+            it.class
+        }).unique()
         // ------------ STOP EDITING HERE  ----------------------
         assert uniqueTypes == [Integer, String]
     }
@@ -64,8 +70,10 @@ class Koan06 extends GroovyTestCase {
         // under the src directory
         int count = 0
         // ------------ START EDITING HERE ----------------------
-
-
+        new File('src').eachFileRecurse {
+            if (it.isFile() && it.getText().contains('Lorem'))
+                count++
+        }
         // ------------ STOP EDITING HERE  ----------------------
         assert count == 3
 
@@ -76,8 +84,16 @@ class Koan06 extends GroovyTestCase {
         // range objects, store all the prime numbers between 200 and 250 in the target variable
         def primesBetween200And250 = []
         // ------------ START EDITING HERE ----------------------
-
-
+        def nonPrimes = []
+        def range = 200..250
+        range.each { possiblePrime ->
+            (2..possiblePrime - 1).each { divisor ->
+                if (possiblePrime % divisor == 0) {
+                    nonPrimes << possiblePrime
+                }
+            }
+        }
+        primesBetween200And250 = range - nonPrimes
         // ------------ STOP EDITING HERE  ----------------------
         assert primesBetween200And250 == [211, 223, 227, 229, 233, 239, 241]
 
